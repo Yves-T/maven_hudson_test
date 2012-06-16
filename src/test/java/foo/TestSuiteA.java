3d0da486007dd37890f46679b47af1243b3ce55b
Yves <yves.talboom@gmail.com>
2012-06-16 17:15:12 +0200

    test suit

---
+++ src/test/java/foo/TestSuiteA.java
@@ -0,0 +1,11 @@
+package foo;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(value = Suite.class)
+@SuiteClasses(value = { AppTest.class })
+public class TestSuiteA {
+
+}
